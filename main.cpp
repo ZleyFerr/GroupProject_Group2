50fefda34d198de9ae8490e063cf5c2cf52ef633
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-04-20 18:09:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <windows.h>
-#include "quadrangle_methods.h"
-#include "quadrangle.h"
+#include "menu.h"
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	setlocale(LC_ALL, "Ru");
-
-	Quadrangle<double> deffault_test;
-	deffault_test.set_sides();
-	deffault_test.change_sides();
-	deffault_test.show();
-	deffault_test.S_calculate();
-	deffault_test.P_calculate();
+    menu();
 }
